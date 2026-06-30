52567cc7f7546e6aa4df603d58bed0ceffe7c1b1
eyob <eyob.tadesse@astu.edu.et>
2026-06-30 21:31:51 +0300

    text field for quantity and dropdownbutton for category is added

--- lib/widgets/new_list.dart
+++ lib/widgets/new_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/data/categories.dart';
 
 class NewList extends StatefulWidget {
   const NewList({super.key});
@@ -22,6 +23,37 @@ class _NewListState extends State<NewList> {
               TextFormField(
                 maxLength: 50,
                 decoration: InputDecoration(label: Text("Name")),
+                validator: (value) {
+                  return '';
+                },
+              ),
+              Row(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(label: Text('Quantity')),
+                    initialValue: '1',
+                  ),
+                  SizedBox(width: 8),
+                  DropdownButtonFormField(
+                    items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              SizedBox(width: 6),
+                              Text(category.value.title),
+                            ],
+                          ),
+                        ),
+                    ],
+                    onChanged: onChanged,
+                  ),
+                ],
               ), //instead of textfield we add testformfield()because we are using form
             ],
           ),
